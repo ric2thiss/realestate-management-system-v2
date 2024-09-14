6ace9e7eaf678941457b66d635210f90a4ac2a02
Ric Charles Paquibot <ric2thiss@gmail.com>
2024-09-14 22:45:30 +0800

    saved the latest changes

--- script/index.js
+++ script/index.js
@@ -1,48 +1,41 @@
 
-// document.querySelector('body').style.backgroundSize = "cover";
-// document.querySelector('body').style.backgroundRepeat = "no-repeat";
-
-// let bg = 1;
-
-// setInterval(() => {
-//     console.log(bg)
-//     document.querySelector('body').style.backgroundImage = `url('assets/${bg}.png')`;
-//     bg++;
-//     if(bg > 3){
-//         bg = 1;
-//     }
-// }, 3000);
-
-
-
 let error;
 
 function validate(event) {
     event.preventDefault();
     
-    const fname = document.myForm.fname.value;
-    
-    if (!filterInput(fname)) {
-        console.log(error);
-        document.myForm.fname.style.border = "1px solid red";
-    }else{
-        document.myForm.fname.style.border = "";
+    const fname = document.form.firstname.value;
+
+    console.log(fname)
+    if(fname == ""){
+        alert("Please enter your first name");
+    }else if(fname === null){
+        alert("Please enter your first name");
+    }else if(fname =="  "){
+        alert("Please remove the space");
     }
     
-    return false;
+    // if (!filterInput(fname)) {
+    //     console.log(error);
+    //     document.myForm.fname.style.border = "1px solid red";
+    // }else{
+    //     document.myForm.fname.style.border = "";
+    // }
+    
+    // return false;
 }
 
-function filterInput(str) {
-    if (str == null || str.trim() === "") {
-        error = "Name must not be empty!";
-        return false;
-    }
+// function filterInput(str) {
+//     if (str == null || str.trim() === "") {
+//         error = "Name must not be empty!";
+//         return false;
+//     }
 
-    if (/\d/.test(str)) {
-        error = "Name must not contain numbers!";
-        return false;
-    }
+//     if (/\d/.test(str)) {
+//         error = "Name must not contain numbers!";
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 

40af467905ddec8189c560682111086bd33759c0
Ric Charles Paquibot <ric2thiss@gmail.com>
2024-09-15 07:49:00 +0800

    added some js for restriction practicing it

--- script/index.js
+++ script/index.js
@@ -5,15 +5,57 @@ function validate(event) {
     event.preventDefault();
     
     const fname = document.form.firstname.value;
+    // const zip = document.form.zip.value;
 
-    console.log(fname)
-    if(fname == ""){
-        alert("Please enter your first name");
-    }else if(fname === null){
-        alert("Please enter your first name");
-    }else if(fname =="  "){
-        alert("Please remove the space");
+    // for(let i = 0; i < fname.length - 2; i++) {
+    //     if(fname[i] == fname[i+1] && fname[i] == fname[i+2]) {
+    //         alert("Three consecutive duplicate characters found");
+    //         return false;
+    //     }
+    // }
+
+
+    // Minimum and Maximum
+    if(fname.length < 1) {
+        alert("Name must be alteast 2 characters")
+    }else if(fname.length > 50){
+        alert("Name must be less than 50 characters")
+    }else{
+        alert("Name is valid");
     }
+
+    console.log(fname.search(/\d/));    
+    // Contain a number
+    // if(fname.search(/\d/) == -1) {
+    //     alert("Name must contain a number");
+    // }
+
+    // Three consecutive duplicate characters found
+    let consecutiveCount = 1;
+
+    for (let i = 1; i < fname.length; i++) {
+        if (fname[i] === fname[i - 1]) {
+            consecutiveCount++;
+            if (consecutiveCount === 3) {
+                alert("Three consecutive duplicate characters found");
+                return false;
+            }
+        } else {
+            consecutiveCount = 1; 
+        }
+    }
+
+    
+    // console.log(fname)
+    // if(fname == ""){
+    //     alert("Please enter your first name");
+    // }else if(fname === null){
+    //     alert("Please enter your first name");
+    // }else if(fname =="  "){
+    //     alert("Please remove the space");
+    // }else{
+    //     alert("First name is valid");
+    // }
     
     // if (!filterInput(fname)) {
     //     console.log(error);
@@ -38,4 +80,48 @@ function validate(event) {
 
 //     return true;
 // }
+
+// function onkeyup(event){
+//     console.log(event.target.value)
+    // const zip = document.getElementById("zip").value;
+    // fetch(`http://api.zippopotam.us/ph/${zip}`)
+    // .then(res => res.json())
+    // .then(data =>{
+    //     console.log(data.places[0]['place name'])
+
+    // })
+// }
+
+document.getElementById('zip').addEventListener('keyup', function(event) {
+    // console.log(event.target.value);
+    const zip = document.getElementById("zip").value;
+    if(zip.length >= 4){
+        fetch(`http://api.zippopotam.us/ph/${event.target.value}`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data.places[0]['place name'])
+            document.getElementById('city').value = data.places[0]['place name'];
+        })
+    }
+});
+
+// document.getElementById('zip').addEventListener('keyup', function(event) {
+//     const zip = event.target.value;
+
+//     if (zip.length >= 4) {  // Adjust length based on your requirements
+//         fetch(`http://api.zippopotam.us/ph/${zip}`)
+//         .then(res => res.json())
+//         .then(data => {
+//             if (data.places && data.places.length > 0) {
+//                 document.getElementById('city').value = data.places[0]['place name'];
+//             }
+//         })
+//         .catch(err => {
+//             console.error('Error:', err);
+//             document.getElementById('city').value = '';
+//         });
+//     }
+// });
+
+
 
